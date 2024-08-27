cef06a1506957365883cd863b49f382c85512568
Amruthakrishna-P <31767534+amruthakrishnap@users.noreply.github.com>
2024-08-27 23:15:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
-
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join('src', 'templates'))
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
