9cd6673c210df3bafb14281456ad05e0d5e3047c
Amruthakrishna-P <31767534+amruthakrishnap@users.noreply.github.com>
2024-09-04 18:58:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/embed', methods=['GET'])
-def embed_code():
-    extension_url = request.args.get('extension_url')
-    if not extension_url:
-        return jsonify({'error': 'Missing extension URL'}), 400
-    
-    embed_code = f'<iframe src="https://freelance-coders.vercel.app/?extension_url={extension_url}" width="800" height="600" frameborder="0" allowfullscreen></iframe>'
-    
-    return jsonify({'embed_code': embed_code})
 
 
